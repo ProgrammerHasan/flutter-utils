5d17388c0977955853ad254c083fcd59c2f3ac0c
ProgrammerHasan <mehedihassan8604@gmail.com>
2021-05-04 08:52:37 +0600

    arNumToEn

--- lib/utils.dart
+++ lib/utils.dart
@@ -99,16 +99,16 @@ String bnNumToEn(int number) {
 // arabic number to english
 String arNumToEn(int number) {
   return number.toString()
-      .replaceAll('0', '۰')
-      .replaceAll('1', '۱')
-      .replaceAll('2', '۲')
-      .replaceAll('3', '۳')
-      .replaceAll('4', '۴')
-      .replaceAll('5', '۵')
-      .replaceAll('6', '۶')
-      .replaceAll('7', '۷')
-      .replaceAll('8', '۸')
-      .replaceAll('9', '۹');
+      .replaceAll('۰', '0')
+      .replaceAll('۱', '1')
+      .replaceAll('۲', '2')
+      .replaceAll('۳', '3')
+      .replaceAll('۴', '4')
+      .replaceAll('۵', '5')
+      .replaceAll('۶', '6')
+      .replaceAll('۷', '7')
+      .replaceAll('۸', '8')
+      .replaceAll('۹', '9');
 }
 
 // Check internet connection
